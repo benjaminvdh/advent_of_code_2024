65db1894f7c2c11467bd0bf129598cdc25438fab
benjaminvdh <4235118+benjaminvdh@users.noreply.github.com>
2024-12-04 07:55:53 +0100

    Day 3 part 2

--- src/Day3.hs
+++ src/Day3.hs
@@ -5,12 +5,19 @@ import Data.List
 
 main = solve part1 part2
 
-part1 input = multAll input
-part2 _ = "N/A"
+part1 = multAll
+part2 = multAllMaybe
 
 multAll ('m':xs) = parseMul xs + multAll xs
 multAll (_:xs) = multAll xs
 multAll [] = 0
+
+multAllMaybe ('m':xs) = parseMul xs + multAllMaybe xs
+multAllMaybe l@(_:xs) = if "don't()" `isPrefixOf` l then skipInput l else multAllMaybe xs
+multAllMaybe [] = 0
+
+skipInput l@(x:xs) = if "do()" `isPrefixOf` l then multAllMaybe xs else skipInput xs
+skipInput [] = 0
 
 parseMul :: String -> Int
 parseMul xs = if "ul(" `isPrefixOf` xs then
